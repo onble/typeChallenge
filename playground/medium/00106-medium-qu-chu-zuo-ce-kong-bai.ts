4d5aaa91a5e1265574c78811edbcae3bf5bb8655
“onble” <yaseeeenoble@gmail.com>
2026-05-19 10:35:17 +0800

    TrimLeft

--- playground/medium/00106-medium-qu-chu-zuo-ce-kong-bai.ts
+++ playground/medium/00106-medium-qu-chu-zuo-ce-kong-bai.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type TrimLeft<S extends string> = any
+type TrimLeft<S extends string> = S extends `${' ' | '\n' | '\t'}${infer R}` ? TrimLeft<R> : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
