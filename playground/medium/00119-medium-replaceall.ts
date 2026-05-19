a97cc43de9234d4bc5d3605e9ceb3d675800294a
“onble” <yaseeeenoble@gmail.com>
2026-05-19 10:50:02 +0800

    ReplaceAll

--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends '' ? S : S extends `${infer V}${From}${infer R}` ? `${V}${To}${ReplaceAll<R, From, To>}` : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
