cfbe96b95ee9f8372cf38751a3788c791c8d1acf
“onble” <yaseeeenoble@gmail.com>
2026-05-20 15:01:10 +0800

    StringToUnion

--- playground/medium/00531-medium-string-to-union.ts
+++ playground/medium/00531-medium-string-to-union.ts
@@ -19,7 +19,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type StringToUnion<T extends string> = any
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}` ? First | StringToUnion<Rest> : never;
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
