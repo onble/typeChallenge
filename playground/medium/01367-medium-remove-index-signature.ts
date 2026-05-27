f35e6e82fdc3c581012260250f7c71121b1144d0
“onble” <yaseeeenoble@gmail.com>
2026-05-27 14:28:33 +0800

    RemoveIndexSignature

--- playground/medium/01367-medium-remove-index-signature.ts
+++ playground/medium/01367-medium-remove-index-signature.ts
@@ -23,7 +23,15 @@
 
 /* _____________ 你的代码 _____________ */
 
-type RemoveIndexSignature<T> = any
+// type RemoveIndexSignature<T> = {
+//   [K in keyof T as string extends K ? never : number extends K ? never : symbol extends K ? never : K]: T[K]
+// }
+
+
+type RemoveIndexSignature<T, P = PropertyKey> = {
+  [K in keyof T as P extends K ? never : K extends P ? K : never]: T[K]
+}
+
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
