c448de829e160c2dbf93189f446b03bcc092b7d6
“onble” <yaseeeenoble@gmail.com>
2026-05-27 16:04:08 +0800

    MinusOne

--- playground/medium/02257-medium-minusone.ts
+++ playground/medium/02257-medium-minusone.ts
@@ -19,7 +19,21 @@
 
 /* _____________ 你的代码 _____________ */
 
-type MinusOne<T extends number> = any
+type ParseInt<T extends string> = T extends `${infer Digit extends number}` ? Digit : never
+type ReverseString<S extends string> = S extends `${infer First}${infer Rest}` ? `${ReverseString<Rest>}${First}` : ''
+type RemoveLeadingZeros<S extends string> = S extends '0' ? S : S extends `${'0'}${infer R}` ? RemoveLeadingZeros<R> : S
+type InternalMinusOne<
+  S extends string
+> = S extends `${infer Digit extends number}${infer Rest}` ?
+  Digit extends 0 ?
+  `9${InternalMinusOne<Rest>}` :
+  `${[9, 0, 1, 2, 3, 4, 5, 6, 7, 8][Digit]}${Rest}` :
+  never
+type MinusOne<T extends number> = ParseInt<RemoveLeadingZeros<ReverseString<InternalMinusOne<ReverseString<`${T}`>>>>>
+type test = MinusOne<9007199254740992>
+
+
+
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
