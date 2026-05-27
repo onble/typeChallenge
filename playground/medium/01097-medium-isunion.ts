b8c9e10ef8ccd11f2d4ece175f283c241c14d4de
“onble” <yaseeeenoble@gmail.com>
2026-05-27 11:41:56 +0800

    IsUnion

--- playground/medium/01097-medium-isunion.ts
+++ playground/medium/01097-medium-isunion.ts
@@ -20,7 +20,47 @@
 
 /* _____________ 你的代码 _____________ */
 
-type IsUnion<T> = any
+type IsUnionImpl<T, C extends T = T> = (T extends T ? C extends T ? true : unknown : never) extends true ? false : true;
+// type IsUnion<T> = IsUnionImpl<T>;
+
+// type IsUnion<U, U1 = U> = [U] extends [never] ? false : (U extends any ? [U1] extends [U] ? false : true : never);
+
+// TypeScript 有一个分布式条件类型特性：
+// 当联合类型进入 T extends any 时，会自动拆分成每一个类型单独判断
+// 比如：string | number 进入 U extends any→ 先判断 string extends any→ 再判断 number extends any→ 最后把结果合并成联合类型
+// 而非联合类型（如 string）不会拆分，只会执行一次。
+
+// type IsUnion<U, U1 = U> = Equal<(U extends any ? U1 extends U ? true : false : false), boolean>;
+
+type falseFlase = false | false;
+type trueTrue = true | true;
+type test = trueTrue extends falseFlase ? true : false;
+type test2 = true | true extends boolean ? true : false;
+type test3 = true | false extends boolean ? true : false;
+type test4 = true | false;
+type test5 = boolean extends false ? true : false;
+type test6 = boolean extends true ? true : false;
+// 只要是相同字面量的联合，TS 会自动向上合并成基础类型！
+
+// way1: length of the union
+// relate to a hard challenge: UnionToTuple
+// 将 U 转换为 Tuple，然后判断其长度即可
+type U2I<U> = (U extends any ? (u: U) => any : never) extends (i: infer I) => any ? I : never
+type Last<U> = U2I<U extends any ? () => U : never> extends () => infer R ? R : never
+type ToTuple<U> = [U] extends [never] ? [] : [Last<U>, ...ToTuple<Exclude<U, Last<U>>>]
+type IsUnion<U, Count = ToTuple<U>['length']> = Count extends 0 ? false : Count extends 1 ? false : true;
+
+type U2IGo<U> = U extends any ? () => U : never
+type testU2I = U2I<string | number>
+type testU2I2 = U2I<{ a: string } | { a: number }>
+type testU2I3 = U2I<U2IGo<string | number>>
+type testLast1 = Last<string | number>;
+type myLast<U> = U extends () => infer R ? R : never;
+type testLast = myLast<testU2I3>;
+type testToTuple = ToTuple<string | number>;
+type testIsUnion = IsUnion<string | number>;
+
+
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
