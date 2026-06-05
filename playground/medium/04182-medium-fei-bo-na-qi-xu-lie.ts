e7adddd23fc573a63c3383461e8a5f261db35481
“onble” <yaseeeenoble@gmail.com>
2026-06-05 16:58:47 +0800

    Fibonacci

--- playground/medium/04182-medium-fei-bo-na-qi-xu-lie.ts
+++ playground/medium/04182-medium-fei-bo-na-qi-xu-lie.ts
@@ -21,7 +21,16 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Fibonacci<T extends number> = any
+type Fibonacci<
+  T extends number,
+  No extends 1[] = [1, 1, 1],
+  N_2 extends 1[] = [1],
+  N_1 extends 1[] = [1]
+> = T extends 1 | 2
+  ? 1
+  : T extends No['length']
+  ? [...N_2, ...N_1]['length']
+  : Fibonacci<T, [...No, 1], N_1, [...N_2, ...N_1]>
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
